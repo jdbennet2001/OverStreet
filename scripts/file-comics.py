4b44341c7e895efaf2943872bbee2abbd059c664
jdbennet2001 <jdbennet2001@yahoo.com>
2021-06-08 22:54:35 -0400

    Finish filing script

--- scripts/file-comics.py
+++ scripts/file-comics.py
@@ -7,8 +7,7 @@ import sys
 import shutil
 import argparse
 
-from os import path, listdir
-
+from os import path, listdir, makedirs
 
 proj_path = path.abspath('.')
 sys.path.append(proj_path)  # VSCode hackery, ensure project relative imports work
@@ -25,6 +24,9 @@ PUBLISHER_DIRECTORIES = ['DC Comics', 'Marvel', 'Other']
 
 def execute(catalog):
 
+    # 
+    volumes = getVolumes(catalog)
+
     # Find comics that need to be filed
     comics = waiting_comics(catalog)
 
@@ -37,17 +39,65 @@ def execute(catalog):
 
         tags = get_tags(comic)
         if ( tags['publisher'] == 'DC Comics'):
-            move(comic, DC_series, path.join(catalog, 'DC Comics'))
+            move(comic, DC_series, path.join(catalog, 'DC Comics'), tags)
         elif ( tags['publisher'] == 'DC Comics'):
-            move( comic, Marvel_series, path.join(catalog, 'Marvel') )
+            move( comic, Marvel_series, path.join(catalog, 'Marvel'), tags )
         else:
-            move(comic, Other_series, path.join(catalog, 'Other') )
-
+            move(comic, Other_series, path.join(catalog, 'Other'), tags )
 
     pass
 
-def move(comic, series, directory):
-    pass
+# Build a dictionary of volume - location for all known volumes
+def getVolumes(catalog):
+
+    comics = contents(catalog)
+    comics = [x for x in comics if volumeNumber(x) is not None]
+
+    volumes = {}
+
+    for comic in comics:
+        volume = volumeNumber(comic)    
+        volumes[volume] = path.dirname(comic)
+
+    return volumes
+
+
+# Returns the volume number from a given comic's parent directory
+def volumeNumber(comic):
+    dir = path.basename( path.dirname(comic) )
+    tokens = dir.split( ' ' )
+    volumes = [x for x in tokens if x.startswith('(') and x.endswith(')')]
+    if ( len(volumes) == 1):
+        volume = volumes[0].lstrip('(').rstrip(')')
+        return volume
+
+# Basic filing logic
+def move(comic, series, directory, tags, volumes):
+    volume_id = tags['volume_id']
+    volume_name = tags['volume_name']
+    volume_start = tags['volume_start']
+    basename = path.basename(comic)
+
+    # Use Volume ID
+    if volume_id in volumes:
+        volume_directory = volumes[volume_id]
+        target = path.join( volume_directory, basename)
+        return mv( comic, target )
+
+    # Use series
+    series_dir = next(folder for folder in series if volume_name in folder)
+    if series_dir is not None:
+        target = path.join( series_dir, basename)
+        return mv( comic, target )
+
+    # Generate new entry
+    target = path.join( directory, f'{directory}/{volume_name} {volume_start} ({volume_id}/{basename}' )
+    mv( comic, target )
+
+    
+
+def mv(source, destination):
+    print( f' ==> {source} ==> {destination}')
 
 # Return a list of all comics in the queue
 def waiting_comics(catalog):
